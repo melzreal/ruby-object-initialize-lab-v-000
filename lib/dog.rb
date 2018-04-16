8f1870f714cb510b1dc751e9dfdde488d8f04ddb
Melissa Real <melissa.real@hotmail.com>
2018-04-16 19:40:33 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog 
+  def initialize(name, breed = "Mutt")
+    @name = name
+    @breed = breed 
+  
+  end
+  
+  def name
+    @name 
+  end 
+  
+  def dog=(name)
+   @name = name 
+ end
+ 
+end 
+
